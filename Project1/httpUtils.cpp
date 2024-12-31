78b099fecd956b48cefcae53cd980e8d16221505
Noam Kontorovich <150962340+NoamKont@users.noreply.github.com>
2024-12-31 18:43:35 +0200

    Head working + change structure of function htmlToString by adding 2 new function

--- Project1/httpUtils.cpp
+++ Project1/httpUtils.cpp
@@ -56,7 +56,63 @@ void doGet(Request request, string& response) {
 	response = htmlToString(nameFile);
 }
 
-string htmlToString(string fileName) {
+//string htmlToString(string fileName) {
+//	string file_path = "C:\\temp\\" + fileName;
+//	ifstream file(file_path);
+//	if (!file.is_open()) {
+//		return "<html><body><h1>404 Not Found</h1></body></html>";
+//	}
+//	ostringstream content;
+//	content << file.rdbuf();
+//	std::ostringstream response_stream;
+//	response_stream << "HTTP/1.1 200 OK\r\n"
+//		<< "Content-Type: text/html\r\n"
+//		<< "Content-Length: " << content.str().size() << "\r\n"
+//		<< "\r\n"
+//		<< content.str();
+//	return response_stream.str();
+//}
+
+string createStatusLine(int statusCode) {
+	string statusLine = "HTTP/1.1 ";
+	switch (statusCode) {
+	case 200:
+		statusLine += "200 OK";
+		break;
+	case 404:
+		statusLine += "404 Not Found";
+		break;
+	case 500:
+		statusLine += "500 Internal Server Error";
+		break;
+	}
+	return statusLine;
+}
+
+string createResponseHeader(string fileName, string contentType) {
+	string file_path = "C:\\temp\\" + fileName;
+	ifstream file(file_path);
+	int statusCode;
+	ostringstream content;
+	
+	if (!file.is_open()) {
+		statusCode = 404;
+		content << "<html><body><h1>404 Not Found</h1></body></html>";
+	}
+	else {
+		statusCode = 200;
+		content << file.rdbuf();
+	}
+
+	std::ostringstream response_stream;
+	response_stream << createStatusLine(statusCode) << "\r\n"
+	<< "Content-Type: " << contentType << "\r\n"
+	<< "Content-Length: " << content.str().size() << "\r\n"
+	<< "\r\n";
+	return response_stream.str();
+}
+
+string createBody(string fileName) {
 	string file_path = "C:\\temp\\" + fileName;
 	ifstream file(file_path);
 	if (!file.is_open()) {
@@ -64,14 +120,25 @@ string htmlToString(string fileName) {
 	}
 	ostringstream content;
 	content << file.rdbuf();
-	std::ostringstream response_stream;
-	response_stream << "HTTP/1.1 200 OK\r\n"
-		<< "Content-Type: text/html\r\n"
-		<< "Content-Length: " << content.str().size() << "\r\n"
-		<< "\r\n"
-		<< content.str();
-	return response_stream.str();
+	return content.str();
 }
 
+string htmlToString(string fileName) {
+	string response = createResponseHeader(fileName,"text/html");
+	response += createBody(fileName);
+	return response;
+}
 
+void doHead(Request request, string& response) {
+	if (request.url != "/index.html") {
+		response = "HTTP/1.1 404 Not Found\r\nContent-Length: 13\r\n\r\n404 Not Found";
+		return;
+	}
+	string lang = request.queryParams["lang"];
+	string nameFile = "index-";
+	set <string> langSet = { "en", "fr", "he" };
+	nameFile += langSet.find(lang) != langSet.end() ? lang : "en";
+	nameFile += ".html";
+	response = createResponseHeader(nameFile, "text/html");
+}
 
